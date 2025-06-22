fa8a8314005970bed49c9e9cd8b75f2641a16888
hugorodriguesqw <mailvitorhugosr@gmail.com>
2025-06-22 14:55:26 -0300

    awesome is awesome

--- src/components/awesome.tsx
+++ src/components/awesome.tsx
@@ -6,11 +6,15 @@ export function AwesomeBox(props: { className?: HTMLAttributes<HTMLDivElement>["
         const loadParticles = async () => {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             const particlesJS = (window as any).particlesJS;
-            particlesJS.load('awesome-box',  '/particles/prefab/awesome-box.json', () => {
+            particlesJS.load('awesome-box', '/particles/prefab/awesome-box.json', () => {
                 console.log('Particles loaded');
             });
 
             particlesJS.load('awesome-box2', '/particles/prefab/awesome-box2.json', () => {
+                console.log('Particles loaded');
+            });
+
+            particlesJS.load('awesome-box3', '/particles/prefab/awesome-box3.json', () => {
                 console.log('Particles loaded');
             });
 
@@ -24,8 +28,9 @@ export function AwesomeBox(props: { className?: HTMLAttributes<HTMLDivElement>["
     return (
         <>
             <script src="particles/particles.js" defer />
-            <div id="awesome-box" className={twMerge("absolute top-0 left-0 w-full h-full", props.className)}></div>
+            <div id="awesome-box3" className={twMerge("absolute top-0 left-0 w-full h-full", props.className)}></div>
             <div id="awesome-box2" className={twMerge("absolute top-0 left-0 w-full h-full", props.className)}></div>
+            <div id="awesome-box" className={twMerge("absolute top-0 left-0 w-full h-full", props.className)}></div>
         </>
     );
 }
